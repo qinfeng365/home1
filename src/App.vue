a5ccd3a55daa8fedbde0b166a22e83aba91648a1
qinfeng365 <111269552+qinfeng365@users.noreply.github.com>
2023-09-27 21:45:39 +0800

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -76,7 +76,7 @@ onMounted(() => {
   // 屏蔽右键
   document.oncontextmenu = () => {
     ElMessage({
-      message: "为了浏览体验，本站禁用右键",
+      message: "本站禁用右键,QwQ",
       grouping: true,
       duration: 2000,
     });
